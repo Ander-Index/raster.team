da1504319c17157e2a831078901f5a41f4adbefc
Ander Index <51054841+Ander-Index@users.noreply.github.com>
2026-03-23 00:18:24 +0800

    NEW l10n

--- Sub-Sites/Chapbook_Gudie/searcher-c2a407aa.js
+++ Sub-Sites/Chapbook_Gudie/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ba4541d0.js',
+                path_to_root + 'searchindex-d9c3c349.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
